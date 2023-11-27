f75810792e6c08023c53958bf2dbcf4c709164bc
w-modz <46196436+w-modz@users.noreply.github.com>
2023-11-27 12:32:01 +0100

    Add timer1 functionality

--- Clock/Form1.Designer.cs
+++ Clock/Form1.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            label_date = new Label();
+            label_time = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label_date
+            // 
+            label_date.AutoSize = true;
+            label_date.Location = new Point(31, 28);
+            label_date.Name = "label_date";
+            label_date.Size = new Size(184, 41);
+            label_date.TabIndex = 0;
+            label_date.Text = "sample_date";
+            label_date.Click += label1_Click;
+            // 
+            // label_time
+            // 
+            label_time.AutoSize = true;
+            label_time.Location = new Point(31, 105);
+            label_time.Name = "label_time";
+            label_time.Size = new Size(184, 41);
+            label_time.TabIndex = 1;
+            label_time.Text = "sample_time";
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label_time);
+            Controls.Add(label_date);
+            Name = "Form1";
+            Text = "Clock";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label_date;
+        private Label label_time;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Clock/Form1.cs
+++ Clock/Form1.cs
@@ -5,6 +5,20 @@ namespace Clock
         public Form1()
         {
             InitializeComponent();
+            label_time.Text = DateTime.Now.ToString("HH:mm:ss");
+            label_date.Text = DateTime.UtcNow.Date.ToString("dd/MM/yyyy");
+            timer1.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label_time.Text = DateTime.Now.ToString("HH:mm:ss");
+            label_date.Text = DateTime.UtcNow.Date.ToString("dd/MM/yyyy");
         }
     }
 }
